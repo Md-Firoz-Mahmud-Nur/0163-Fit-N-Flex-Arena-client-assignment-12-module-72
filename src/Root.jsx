e6ea24085c6385f505af1ce4d1fa9debb75b0838
firoz.nur <firoznur5@gmail.com>
2024-06-08 19:18:52 +0600

    footer

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,3 +1,4 @@
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 
@@ -6,6 +7,7 @@ const Root = () => {
     <div>
       <Navbar></Navbar>
       root
+      <Footer></Footer>
     </div>
   );
 };
