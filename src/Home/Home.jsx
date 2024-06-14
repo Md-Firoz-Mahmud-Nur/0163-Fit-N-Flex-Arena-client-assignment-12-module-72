4862e448b49e1e6663830533623aee8e2ceb9641
firoz.nur <firoznur5@gmail.com>
2024-06-14 17:07:27 +0600

    Key Features Done

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "./Banner";
+import FeaturedSection from "./FeaturedSection";
 
 const Home = () => {
   return (
     <div className="container mx-auto my-20">
       <Banner></Banner>
+      <FeaturedSection></FeaturedSection>
     </div>
   );
 };
